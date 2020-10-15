ee34ce31288304085f0fb6df7e516bc8b9ceb17c
encarju <justine.encarnacion@oocl.com>
2020-10-15 18:22:35 +0800

    [MarsRover][ENCARJU] test : Add case for given 0,0,S command M return 0,-1,S

--- src/test/java/com/afs/tdd/MarsRoverTest.java
+++ src/test/java/com/afs/tdd/MarsRoverTest.java
@@ -39,4 +39,16 @@ class MarsRoverTest {
         assertEquals(0, marsRover.getCoordinateY());
         assertEquals("E",marsRover.getHeadingDirection());
     }
+
+    @Test
+    void should_return_0_negative_1_S_when_execute_given_0_0_S_command_R() {
+        //Given
+        MarsRover marsRover = new MarsRover(0,0,"S");
+        //When
+        marsRover.executeCommands("M");
+        //Then
+        assertEquals(0, marsRover.getCoordinateX());
+        assertEquals(-1, marsRover.getCoordinateY());
+        assertEquals("S",marsRover.getHeadingDirection());
+    }
 }
