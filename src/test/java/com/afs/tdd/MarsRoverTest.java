95b395f22376e2a3b42ed1ad134125b972f690ba
encarju <justine.encarnacion@oocl.com>
2020-10-15 20:33:01 +0800

    [MarsRover][ENCARJU] test : Add case for given not valid command String return CommandNotDefinedException

--- src/test/java/com/afs/tdd/MarsRoverTest.java
+++ src/test/java/com/afs/tdd/MarsRoverTest.java
@@ -2,9 +2,13 @@ package com.afs.tdd;
 
 import org.junit.jupiter.api.Test;
 
+import org.junit.jupiter.api.function.Executable;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class MarsRoverTest {
+
     @Test
     void should_return_0_1_N_when_execute_given_0_0_N_command_M() {
         //Given
@@ -146,6 +150,18 @@ class MarsRoverTest {
         assertEquals(0, marsRover.getCoordinateX());
         assertEquals(0, marsRover.getCoordinateY());
         assertEquals("N",marsRover.getHeadingDirection());
+    }
+
+    @Test
+    void should_return_exception_when_execute_given_command_string_not_defined() {
+        //Given
+        MarsRover marsRover = new MarsRover(0,0,"W");
+        //When
+        Executable executable = () -> {
+            marsRover.executeCommands("MLMR");
+        };
+        //Then
+        assertThrows(CommandNotDefinedException.class,executable);
     }
 
 }
