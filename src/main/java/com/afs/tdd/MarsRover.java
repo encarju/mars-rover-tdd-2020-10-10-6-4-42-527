a9f534ad6e0eb153d5af494e438f0d7ab28c24b0
encarju <justine.encarnacion@oocl.com>
2020-10-15 21:28:15 +0800

    [MarsRover][ENCARJU] refactor : Update Constants for MarsRover

--- src/main/java/com/afs/tdd/MarsRover.java
+++ src/main/java/com/afs/tdd/MarsRover.java
@@ -8,14 +8,13 @@ public class MarsRover {
     private static final String DELIMITER = "";
     private static final String WEST_DIRECTION = "W";
     private static final int MOVE_ONE_COORDINATE = 1;
-    private static final String TURN_LEFT = "L" ;
+    private static final String TURN_LEFT = "L";
     private static final String TURN_RIGHT = "R";
     private static final String EAST_DIRECTION = "E";
     private static final String SOUTH_DIRECTION = "S";
-    private  int coordinateX;
-    private  int coordinateY;
-    private  String headingDirection;
-
+    private int coordinateX;
+    private int coordinateY;
+    private String headingDirection;
 
 
     public MarsRover(int coordinateX, int coordinateY, String headingDirection) {
@@ -30,80 +29,72 @@ public class MarsRover {
         }
     }
 
-    private void executeCommand(String command) throws CommandNotDefinedException{
-        if(command.equals(MOVE)){
+    private void executeCommand(String command) throws CommandNotDefinedException {
+        if (command.equals(MOVE)) {
             move();
-        }
-        else if(command.equals(TURN_LEFT)){
+        } else if (command.equals(TURN_LEFT)) {
             turnLeft();
-        }
-        else if(command.equals(TURN_RIGHT)){
+        } else if (command.equals(TURN_RIGHT)) {
             turnRight();
-        }
-        else{
+        } else {
             throw new CommandNotDefinedException("Command Not Defined");
         }
     }
 
     private void turnRight() {
-        if(isNorthernDirection()){
-            headingDirection = EAST_DIRECTION;
-        }
-        else if(isSouthernDirection()){
-            headingDirection = WEST_DIRECTION;
-        }
-        else if(isEasternDirection()){
-            headingDirection = SOUTH_DIRECTION;
-        }
-        else if(isWesternDirection()){
-            headingDirection = NORTH_DIRECTION;
+        if (isNorthernDirection()) {
+            setHeadingDirection(EAST_DIRECTION);
+        } else if (isSouthernDirection()) {
+            setHeadingDirection(WEST_DIRECTION);
+        } else if (isEasternDirection()) {
+            setHeadingDirection(SOUTH_DIRECTION);
+        } else if (isWesternDirection()) {
+            setHeadingDirection(NORTH_DIRECTION);
         }
     }
 
     private void turnLeft() {
-        if(isNorthernDirection()){
-            headingDirection = WEST_DIRECTION;
-        }
-        else if(isSouthernDirection()){
-            headingDirection = EAST_DIRECTION;
-        }
-        else if(isEasternDirection()){
-            headingDirection = NORTH_DIRECTION;
-        }
-        else if(isWesternDirection()){
-            headingDirection = SOUTH_DIRECTION;
+        if (isNorthernDirection()) {
+            setHeadingDirection(WEST_DIRECTION);
+        } else if (isSouthernDirection()) {
+            setHeadingDirection(EAST_DIRECTION);
+        } else if (isEasternDirection()) {
+            setHeadingDirection(NORTH_DIRECTION);
+        } else if (isWesternDirection()) {
+            setHeadingDirection(SOUTH_DIRECTION);
         }
     }
 
     private void move() {
-        if(isNorthernDirection()){
+        if (isNorthernDirection()) {
             coordinateY += MOVE_ONE_COORDINATE;
-        }
-        else if(isSouthernDirection()){
+        } else if (isSouthernDirection()) {
             coordinateY -= MOVE_ONE_COORDINATE;
-        }
-        else if(isEasternDirection()){
+        } else if (isEasternDirection()) {
             coordinateX += MOVE_ONE_COORDINATE;
-        }
-        else if(isWesternDirection()){
+        } else if (isWesternDirection()) {
             coordinateX -= MOVE_ONE_COORDINATE;
         }
     }
 
-    private boolean isNorthernDirection(){
-        return headingDirection==NORTH_DIRECTION;
+    private boolean isNorthernDirection() {
+        return headingDirection == NORTH_DIRECTION;
     }
 
-    private boolean isSouthernDirection(){
-        return headingDirection==SOUTH_DIRECTION;
+    private boolean isSouthernDirection() {
+        return headingDirection == SOUTH_DIRECTION;
     }
 
     private boolean isEasternDirection() {
-        return headingDirection==EAST_DIRECTION;
+        return headingDirection == EAST_DIRECTION;
     }
 
     private boolean isWesternDirection() {
-        return headingDirection==WEST_DIRECTION;
+        return headingDirection == WEST_DIRECTION;
+    }
+
+    private void setHeadingDirection(String headingDirection){
+        this.headingDirection = headingDirection;
     }
 
     public int getCoordinateX() {
